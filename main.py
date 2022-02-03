2637bfea4597b37d9fa5a8e75c71ca10ec515fb4
Kazunari Kaizu <kwaizu@gmail.com>
2022-02-03 23:05:44 +0900

    Skip empty lines

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import re
 
 def bigg_models(downloads, artifacts):
     bigg_models_reactions = []
+
     with open(downloads / "bigg_models_reactions.txt") as f:
         reader = csv.reader(f, delimiter='\t')
         next(reader)
         for row in reader:
             bigg_id, name, reaction_string, model_list, database_links, old_bigg_ids = row
             for xref in database_links.split(';'):
+                xref = xref.strip()
+                if xref == "":
+                    continue
                 mobj = re.match('^\s*([^:]+)\s*:\s*(http://identifiers.org/\S+)', xref)
-                assert mobj is not None
+                assert mobj is not None, f"Failed to parse [{xref}]"
                 db_name, db_id = mobj.group(1), mobj.group(2)
                 bigg_models_reactions.append((f"https://identifiers.org/bigg.reaction:{bigg_id}", db_id))
-    
+
     with open(artifacts / "bigg_models_reactions.csv", "w") as f:
         writer = csv.writer(f)
         for row in bigg_models_reactions:
@@ -23,9 +27,9 @@ def bigg_models(downloads, artifacts):
 
 if __name__ == "__main__":
     from pathlib import Path
-    
+
     downloads, artifacts = Path('downloads'), Path('artifacts')
-    
+
     with open(artifacts / "output1.txt", "w") as f:
         f.write("Saluton, Mondo!\n")
 
