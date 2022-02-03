6f733c18fe8841311ea1ae48ad70d68c8bf72a65
Kazunari Kaizu <kwaizu@gmail.com>
2022-02-03 22:40:35 +0900

    Generate bigg_models

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+import csv
+
+
+def bigg_models(downloads, artifacts):
+    bigg_models_reactions = []
+    with open(downloads / "bigg_models_reactions.txt") as f:
+        reader = csv.reader(f, delimiter='\t')
+        next(reader)
+        for row in reader:
+            bigg_id, name, reaction_string, model_list, database_links, old_bigg_ids = row
+            bigg_models_reactions.append((bigg_id, database_links))
+    
+    with open(artifacts / "bigg_models_reactions.csv", "w") as f:
+        writer = csv.writer(f)
+        for row in bigg_models_reactions:
+            writer.writerow(row)
+
+
 if __name__ == "__main__":
-    with open("artifacts/output1.txt", "w") as f:
+    from pathlib import Path
+    
+    downloads, artifacts = Path('downloads'), Path('artifacts')
+    
+    with open(artifacts / "output1.txt", "w") as f:
         f.write("Saluton, Mondo!\n")
+
+    bigg_models(downloads, artifacts)
